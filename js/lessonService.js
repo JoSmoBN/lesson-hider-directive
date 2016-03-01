e61ed17af26498543034fb11f71aa9e048abbf9b
Joseph Nolan <josephbnolan@me.com>
2016-03-01 16:04:27 -0600

    cleaned up code a little

--- js/lessonService.js
+++ js/lessonService.js
@@ -1,7 +1,5 @@
-angular.module('directivePractice')
-  .service('lessonService', function($http) {
-  
-    this.getSchedule = function() {
-      return $http.get('schedule.json');
+angular.module('directivePractice').service('lessonService', function($http) {
+  this.getSchedule = function() {
+    return $http.get('schedule.json');
   }
 })
